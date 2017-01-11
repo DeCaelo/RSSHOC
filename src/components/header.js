403a8e74ea63f509060878145357b9ce1210cc11
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-11 17:13:51 +0100

    route setup

--- src/components/header.js
+++ src/components/header.js
@@ -10,13 +10,13 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
-          <li className="navbar-item">
+          <li className="nav-item">
             <Link to="/">Home</Link>
           </li>
-          <li className="navbar-item">
+          <li className="nav-item">
             <Link to="/resources">Resources</Link>
           </li>
-          <li className="navbar-item">
+          <li className="nav-item">
             {this.authButton()}
           </li>
         </ul>
